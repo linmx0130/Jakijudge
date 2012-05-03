29b93d4f147bf96d3c8bb87630d71e223a288eb6
Sweetdumplings <linmx0130@163.com>
2012-05-03 18:32:38 +0800

    add File_config, TODO :Diff_config.run()

--- base/base.py
+++ base/base.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-
+#  -*- coding: utf-8 -*-
 #  base/base.py
 #  Jaki Judge: Offline OI Judger in Linux
 #  Copyright (c) by Sweetdumplings<linmx0130@163.com> 
@@ -16,13 +16,40 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+
 """
 module base: the base of all the other modules of jaki judge
 It provides jaki with some class which should be used in different modules.
+All the class need a member: _jaki_class_mark. It is a string and it show what it is.
 
 the list of classes the base module supported:
+    File_config
     Diff_config
 """
+    
+class File_config:
+    """
+    This class 5 has members:
+        1.std_input_file
+        2.std_output_file
+        3.input_file
+        4.output_file
+        5.define(std_input_file,std_output_file,output_file,input_file)
+    """
+    
+    def __init__(self,std_input_file,std_output_file,input_file,output_file):
+        self._jaki_class_mark="FILE_CONFIG"
+        self.define(std_input_file,std_output_file,input_file,output_file)
+    
+    def define(self,std_input_file,std_output_file,input_file,output_file):
+        """
+        this function will help user to set up a file config class
+        """
+        self.std_input_file=std_input_file
+        self.std_output_file=std_output_file
+        self.input_file=input_file
+        self.output_file=output_file
+
 class Diff_config:
     """
     The tester will use a object of Diff_config to tell what diff program
@@ -30,12 +57,19 @@ class Diff_config:
     It has two members: 
         1.standard: the standard which the diff to follow
         2.filename: the path of the diff program
+        3.run() 
     """
     def __init__(self,standard=1,file_name="oidiff"):
-        self.standard=1;
-        self.file_name=1;
+        self._jaki_class_mark="DIFF_CONFIG"
+         
+    def run():
+    #TODO
 
+        """
+        This function is used to call the diff program to compare the 
+        answer and return the result.
+        """     
+    
 if __name__=="__main__":
     print("This file can't be called by user directly.")
-    print(base.__doc__)
-
+    print(__doc__)
